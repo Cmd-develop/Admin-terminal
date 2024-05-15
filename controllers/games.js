e3bf439b6a9d043e840c7358423fe764612f1f18
Cmd-develop <150024807+Cmd-develop@users.noreply.github.com>
2024-05-15 21:06:38 +0300

    Дз неделя 3

---
+++ controllers/games.js
@@ -0,0 +1,17 @@
+const sendAllGames = (req, res) => {
+    res.send(req.games);
+};
+
+const sendUpdatedGames = (req, res) => {
+    res.send({
+        games: req.games,
+        updated: req.updatedObject
+    });
+};
+
+module.exports = { sendAllGames, sendUpdatedGames };
+
+module.exports = {
+    sendAllGames,
+    sendUpdatedGames
+}
